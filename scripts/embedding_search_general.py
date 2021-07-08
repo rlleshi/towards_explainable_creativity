92c21c2785223372658550ee50c6b57d65d8f9c6
rlleshi <rejnald.lleshi@gmail.com>
2021-07-08 07:15:07 +0200

    order top embeddings

--- scripts/embedding_search_general.py
+++ scripts/embedding_search_general.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.lib.twodim_base import tri
 import pandas as pd
 
 from tqdm import tqdm
@@ -36,7 +37,7 @@ def parse_args():
     parser.add_argument(
         '--threshold',
         type=float,
-        default=0.5,
+        default=0.25,
         help='threshold for cosine similarity')
     parser.add_argument(
         '--top-k',
@@ -62,16 +63,43 @@ def check_glove(g, node, noun):
 
     return round(cosine_similarity(em1, em2)[0][0], 3)
 
+def has_solution(ground, result):
+    has_solution = []
+    for tup in zip(ground.values, result):
+        # no solution
+        if not tup[1]:
+            has_solution.append(False)
+            continue
+        # no ground solution
+        if str(tup[0]) == 'nan':
+            continue
+
+        # ! why do we need levenshtein here? why won't simple equality work?
+        if any(edit_distance(tup[0].strip(), sol) < 2 for sol in tup[1]):
+            has_solution.append(True)
+        else:
+            has_solution.append(False)
+    return has_solution
 
 def save_output(args, result):
     df = pd.read_csv(args.file, sep=';')
     df['Top Embeddings'] = result
-    df = df[['w1', 'w2', 'w3', 'wans', 'Top Embeddings']]
+    df['has_solution'] = has_solution(df['wans'], result)
+    df = df[['w1', 'w2', 'w3', 'wans', 'has_solution', 'Top Embeddings']]
+
+    try:
+        df['Accuracy'].iloc[-1] = str(100*round(
+            df['has_solution'].value_counts()[True] / (len(df['has_solution']) - 1), 3)) + '%'
+        df = df[['w1', 'w2', 'w3', 'wans', 'has_solution', 'Top Embeddings', 'Accuracy']]
+    except KeyError:
+        pass
+
     df.to_excel(f'top_embeddings_{args.threshold}thr_{args.top_k}topk.xlsx')
 
 
 def main():
     args = parse_args()
+    # TODO Try other embeddings
     g = GloveEmbedding('common_crawl_840', d_emb=300, show_progress=True)
     nouns = list({x.name().split('.', 1)[0] for x in wn.all_synsets('n')})
     global CACHE
@@ -87,8 +115,14 @@ def main():
                     temp.append(noun)
             triple_result.append(temp)
 
+        # pick top-k from the intersection
         intersection = list(set(triple_result[0]) & set(triple_result[1]) & set(triple_result[2]))
-        result.append(', '.join(intersection[:args.top_k]))
+        top_nouns = {noun: 0 for noun in intersection}
+        for noun in intersection:
+            for node in triple:
+                top_nouns[noun] += check_glove(g, node.strip(), noun)
+        top_nouns = sorted(top_nouns.items(), key=lambda x: x[1], reverse=True)
+        result.append(', '.join(noun[0] for noun in top_nouns[:args.top_k]))
 
     save_output(args, result)
 
